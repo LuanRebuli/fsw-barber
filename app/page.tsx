f30049a76c8c03d6e77047c002da384918339365
Luan <yluanpvp@gmail.com>
2024-08-07 15:07:28 -0300

    feat: add Header component and Banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
+import { Button } from "./_components/ui/button"
+import Image from "next/image"
+
 export default function Home() {
-  return <h1 className="text-red-700">Hello World</h1>;
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Luan!</h2>
+        <p>Quarta Feira, 07 de Agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua Busca.." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos Melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
